34ea8c262b96547b2e584c2a5f6b7acaf3f5d716
Michael-Nicoletti <mike.e.nicoletti@gmail.com>
2016-06-04 17:07:26 -0400

    Player Dash on Click
    The player can now dash between targets while staying in the air but
    will eventually start falling to the ground.
    
    Next to add is the drag dash which has been slightly implemented but
    needs to be more ironed out.

--- EndlessUrbNinja/Assets/Scripts/Player Scripts/DashMovement.cs
+++ EndlessUrbNinja/Assets/Scripts/Player Scripts/DashMovement.cs
@@ -5,12 +5,24 @@ public class DashMovement : MonoBehaviour {
 
     //Private Variables
     private Camera gCam;
+    private Vector3 dashTarget;
+    private Vector3 dragDashDirection;
+    private Rigidbody rb;
+    private bool canDragDash = false;
+
+    //Private Variables for the Held Button Dash
+    private float timeButtonHeldDown;
+    private bool buttonHeldDown = false;
+
 
 	// Use this for initialization
 	void Start () {
 
         //Store the main camera so a ray can be sent from the mouses position relative to it
         gCam = Camera.main;
+       
+        //Store the Rigidbody on the player for faster access
+        rb = GetComponent<Rigidbody>();
 
 	}
 	
@@ -20,22 +32,101 @@ public class DashMovement : MonoBehaviour {
         //This is a fast way of saying Mouse 1, touch screen or CTRL
         if (Input.GetButtonDown("Fire1"))
         {
+
+            buttonHeldDown = true;
             //Keeps Update clean by having all the mouse/touch behaviour in another function
             mouseHandler();
+            
+        }
+        //Added for the drag dash fire. It runs the dragDashHandler function as well as tells the game that
+        //The player can no longer dragdash and to stop the held button count down.
+        //@note: Not working yet.
+        if(Input.GetButtonUp("Fire1"))
+        {
+            if (canDragDash)
+            {
+                Debug.Log("Running");
+                //dragDashHandler();
+            }
+            canDragDash = false;
+            buttonHeldDown = false;
+        }
+
+        //Checks the distance between the player and the dash target. Also slows down time so the player has
+        //More time to react to the next point. Needs to be made smoother and needs tweaking
+        //@note: Can be changed and made dynamic so it can check not only
+        //The dash target but also the drag dash target.
+        if(Vector3.Distance(transform.position, dashTarget) < 0.5f)
+        {
+            rb.useGravity = true;
+            Time.timeScale = 0.4f;
+        }
+
+
+        //Starting code for the drag dash
+        if(buttonHeldDown == true)
+        {
+            timeButtonHeldDown++;
+        }
+        if(timeButtonHeldDown > 10)
+        {
+            canDragDash = false;
         }
 
     }
 
     void mouseHandler()
     {
+        timeButtonHeldDown = 0;
+
+        //Resets the time scale so the dash is still fast
+        if (Time.timeScale != 1)
+        {
+            Time.timeScale = 1;
+        }
+
         //Create a Hit object to store the following raycast in
         RaycastHit hit;
         //Send a ray from the mouses current position relative to the camera that is 100 units long 
         //(@note: Not sure if this value matters, need more testing), and stores it in the Raycast Hit object
         if (Physics.Raycast(gCam.ScreenPointToRay(Input.mousePosition), out hit, 100))
         {
-            //Quick test to see if it is hitting only one object specifically.
+            if (hit.collider.CompareTag("Enemy"))
+            {
+                Debug.Log("Target Set");
+
+                //Sets the dash target to the object that was hit as long as it hits an enemy.
+                dashTarget = hit.collider.gameObject.transform.position;
+
+                Debug.Log(hit.collider.gameObject.transform.position);
+                Debug.DrawLine(transform.position, hit.collider.gameObject.transform.position, Color.red, 3);
+
+                //Set no gravity so the player doesnt immediately start flying down, reset the player motion so the dash works properly.
+                rb.useGravity = false;
+                rb.velocity = Vector3.zero;
+
+                //Get the position between the dashTarget and the players current position and multiply it by the 100 scalar to maek it fast
+                rb.AddForce((dashTarget - transform.position) * 100);
+
+                //Allow the system to start the drag dash
+                canDragDash = true;
+                
+
+            }
+            else
+            //Just a debug that shows if the player clicked something that wasnt the enemy. Useful for debugging.
             Debug.Log(hit.collider.name);
         }
     }
+
+    //void dragDashHandler()
+    //{
+    //    dragDashDirection = gCam.(Input.mousePosition);
+
+    //    Debug.DrawLine(transform.position, dragDashDirection, Color.green, 3);
+
+    //    rb.useGravity = false;
+    //    rb.velocity = Vector3.zero;
+    //    rb.AddForce((dragDashDirection - transform.position) * 100);
+    //}
 }
