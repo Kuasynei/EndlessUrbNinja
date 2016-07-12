4c1399f12dc258d8ecb92fd2b62027a5a7b967e8
Michael-Nicoletti <mike.e.nicoletti@gmail.com>
2016-07-11 20:29:10 -0400

    EnemyScript
    Added a script to the enemy that kills them once the player gets to them
    during a dash.

---
+++ EndlessUrbNinja/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] int distanceBuffer;
+
+    private GameObject player;
+
+    //True = Alive, False = Dead
+    bool currentState = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(player)
+        {
+            //Since the hitbox on the enemies is large to account for the player misclicking due to clumbsy hands, the enemy will not die until the 
+            //player is within the distance buffer.
+            if (Vector3.Distance(gameObject.transform.position, player.transform.position) <= distanceBuffer && currentState == true)
+            {
+                Debug.Log("Player killed enemy");
+
+                currentState = false;
+
+                gameObject.SetActive(false);
+                
+            }
+
+        }	
+	}
+
+    void OnTriggerEnter(Collider colInfo)
+    {
+        if (colInfo.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player Within Enemy Radius");
+
+            player = colInfo.gameObject;
+        }
+    }
+
+    public bool isDead()
+    {
+        return currentState;
+    }
+}
